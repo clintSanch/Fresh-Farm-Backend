1bd98fc01215078659c9d2aed55ce0e446265615
clintSanch <clintonodira@gmail.com>
2023-04-10 21:29:51 -0400

    mvc initialisation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on http://localhost:${PORT}`)
 })
 
 module.exports = app;
---
+++ src/Model/Db/pool.js
@@ -0,0 +1,6 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const database_config = {};
