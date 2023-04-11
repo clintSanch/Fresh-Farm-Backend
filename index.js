fc81244e268622c30d481092b9b3a6f32c80016b
clintSanch <clintonodira@gmail.com>
2023-04-11 00:05:56 -0400

    file parsing enabled and initialised

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const postgres = require('pg');
 const router = require('./app');
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
