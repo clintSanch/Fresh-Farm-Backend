cf18b0f9f5b858ad31a79aa2e5141b8fe282533e
clintSanch <clintonodira@gmail.com>
2023-04-12 23:33:27 -0400

    configuring the domains to be called on the server side

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+require("dotenv").config();
 import express from 'express';
 const cors = require('cors');
 const postgres = require('pg');
 const router = require('./app');
-const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
+
+const allowedListDomains = ['http://localhost:4200', 'http://localhost:4000'];
+const corsOptionsDelegate = (req, callback) => {
+    let corsOptions;
+
+    let isDomainAllowed = allowedListDomains.indexOf(req.header('origin')) !== -1;
+
+    if(isDomainAllowed){
+        corsOptions = {origin: true}
+    }else{
+        corsOptions = { origin: false}
+    }
+
+    callback(null, corsOptions);
+}
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+
+app.use(cors(corsOptionsDelegate));
+
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
